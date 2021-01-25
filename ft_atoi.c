d61832038af7754d3c2966e87b5667f608634913
AISNERR <21AISNERR@usergithub.com>
2021-01-26 01:01:20 +0300

    first_commit

---
+++ ft_atoi.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	negative;
+
+	negative = 1;
+	res = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
+	if (*str == '-' || *str == '+')
+		++str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+	}
+	return (res * negative);
+}
+
+int main()
+{
+	char *str;
+
+	str = "12Three45678";
+	printf("ft_atoi: %d\n", ft_atoi(str));
+	printf("ft_atoi: %d\n", ft_atoi("123456"));
+	printf("atoi: %d\n", atoi("123456"));
+	printf("ft_atoi: %d\n", ft_atoi("12Three45678"));
+	printf("atoi: %d\n", atoi("12Three45678"));
+	printf("ft_atoi: %d\n", ft_atoi("Hello World!"));
+	printf("atoi: %d\n", atoi("Hello World!"));
+	printf("ft_atoi: %d\n", ft_atoi("+42 BLAH!"));
+	printf("atoi: %d\n", atoi("+42 BLAH!"));
+	printf("ft_atoi: %d\n", ft_atoi("-42"));
+	printf("atoi: %d\n", atoi("-42"));
+	return 0;
+}
