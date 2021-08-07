a2dac9b76bad8c67cf8a94e5b68c50d59a41bdee
AISNERR <21AISNERR@usergithub.com>
2021-08-07 15:29:58 +0300

    dnjkfn

---
+++ rush/file1.c
@@ -0,0 +1,30 @@
+void	ft_putchar(char c);
+
+/*
+** (w == 1 || w == width) = start or end of line
+** (h == 1 || h == height) = top or bottom of colunm
+*/
+
+void	rush(int width, int height)
+{
+	int	w;
+	int h;
+
+	h = 0;
+	while (h++ < height)
+	{
+		w = 0;
+		while (w++ < width)
+		{
+			if (h == 1 && (w == 1 || w == width))
+				ft_putchar('A');
+			else if (h == height && (w == 1 || w == width))
+				ft_putchar('C');
+			else if ((w == 1 || w == width) || (h == 1 || h == height))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+	}
+}
---
+++ rush/file2.c
@@ -0,0 +1,9 @@
+void	ft_putchar(char c);
+
+void	rush(int width, int height);
+
+int		main(void)
+{
+	rush(4, 4);
+	return (0);
+}
